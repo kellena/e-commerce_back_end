6b82172a6e68e307327e81bd80426e9dd6212cee
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-05-12 11:09:50 -0500

    filled out product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const getProduct = await Product.findAll({
+      include: [Category, Tag]
+    });
+    res.status(200).json(getProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const getProduct = await Product.findByPk(req.params.id,{
+      include: [Category, Tag]
+    });
+    res.status(200).json(getProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -91,6 +107,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy(
+      { where: { id: req.params.id } }
+    );
+    res.status(200).json(deleteProduct);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
